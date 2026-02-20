17d551651997a7b0ade4b5259557e14f3da18b0d
Afrain Da Silva Calixto <calixtoafrain@gmail.com>
2026-02-20 11:40:23 -0300

    Adicionando a classe User

---
+++ CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class User : BaseEntity
+    {
+        public string? Name { get; private set; }
+        public string? Email { get; private set; }
+
+        public User(string? name, string? email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
