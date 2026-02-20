4db81797aa71e34c3a7f51a5d8a74b888d8b9d58
Afrain Da Silva Calixto <calixtoafrain@gmail.com>
2026-02-20 12:34:08 -0300

    Adicionando a interface IUserRepository

--- CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
+++ CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
@@ -10,9 +10,13 @@ namespace CleanArchitecture.Domain.Interfaces
     public interface IBaseRepository<T> where T : BaseEntity
     {
         Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
         Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellation);
+
         Task AddAsync(T entity);
+
         Task UpdateAsync(T entity);
+
         Task DeleteAsync(Guid id);
     }
 }
