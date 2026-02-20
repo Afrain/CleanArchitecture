44f4f868be8a777b41e9aa35f5fd48bad9f0a622
Afrain Da Silva Calixto <calixtoafrain@gmail.com>
2026-02-20 12:51:53 -0300

    Ajustes no projeto

--- CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
+++ CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
@@ -1,22 +1,14 @@
 ﻿using CleanArchitecture.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CleanArchitecture.Domain.Interfaces
 {
     public interface IBaseRepository<T> where T : BaseEntity
     {
         Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-
         Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellation);
-
         Task AddAsync(T entity);
-
         Task UpdateAsync(T entity);
-
         Task DeleteAsync(Guid id);
     }
 }
