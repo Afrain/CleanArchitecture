1fc4eb4869ba09378f90b67e0b819244aec42006
Afrain Da Silva Calixto <calixtoafrain@gmail.com>
2026-02-20 15:19:58 -0300

    Implementando DbContext com EntityFremewordCore e adicionando referencias entre os projetos

---
+++ CleanArchitecture.Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
